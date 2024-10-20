24b153bcf7aff3ee3762da16b084721f8ade4b80
Samet Güçlü <sametgcl22@gmail.com>
2024-10-20 23:21:23 +0300

    Add files via upload

---
+++ Ödev 1/Program.cs
@@ -0,0 +1,69 @@
+﻿
+//İki Sayinin Farkını Hesaplayan C# Kodu.
+/*
+Console.WriteLine("Birinci sayiyi giriniz:");
+int sayi = int.Parse(Console.ReadLine());
+Console.WriteLine("ikinci sayiyi giriniz:");
+int sayi2  = int.Parse(Console.ReadLine());
+
+int sonuc = sayi - sayi2;
+Console.WriteLine("sonucunuz: " + sonuc);
+Console.ReadKey();
+*/
+
+
+//5 Sayısının Karesini Hesaplayan C# Kodu.
+/*
+int a = 5;
+int sonuc = a * a;
+Console.WriteLine("5 Sayısının Karesi:" + sonuc);
+Console.ReadKey();
+*/
+
+//10 Sayısının 3'e Bölümünden Kalanı Bulan C# Kodu.
+/*
+int e = 10;
+int b = 10 % 3;
+Console.WriteLine("10 Sayısının 3 İle Bölümünden Kalan:" + b);
+Console.ReadKey();
+*/
+
+//Klavyeden Girilen 4 Sayının Toplamını Ve Carpımını Bulan c# Kodu.
+/*
+Console.WriteLine("Birinci Sayiyi Giriniz:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("İkinci Sayiyi Giriniz:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Üçüncü Sayiyi Giriniz:");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("Dördüncü Sayiyi Giriniz:");
+int d = int.Parse(Console.ReadLine());
+int sonuc_toplam = a + b + c + d;
+Console.WriteLine("Dört Sayının Toplamı:" + sonuc_toplam);
+int sonuc_carpım = a * b * c * d;
+Console.WriteLine("Dört Sayının Carpımı:" + sonuc_carpım);
+*/
+
+//Klavyeden Girilen İki Sayının Bölümünü Bulan C# Kodu.
+/*
+Console.WriteLine("Birinci Sayiyi Giriniz:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("İkinci Sayiyi Giriniz:");
+int b = int.Parse(Console.ReadLine());
+int sonuc_bölme = a / b;
+Console.WriteLine("İki Sayının Bölme İşlemi Sonucu:" + sonuc_bölme);
+Console.ReadKey();
+*/
+
+//Klavyeden Kenarları Girilen Dikdörtgenler Prizmasının Hacmini Hesaplayan C# Kodu.
+/*
+Console.WriteLine("Dikdörtgenler Prizmasının Uzunluğunu Giriniz:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Dikdörtgenler Prizmasının Genişliğini Giriniz:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Dikdörtgenler Prizmasının Yüksekliğini Giriniz:");
+int c = int.Parse(Console.ReadLine());
+int sonuc_hacim = a * b * c;
+Console.WriteLine("Dikdörtgen Prizmasının Hacmi:" + sonuc_hacim);
+Console.ReadKey();
+*/
